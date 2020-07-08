acac42ab13ec2aeeb5f82e67e4079884fdbd67f0
Alan Galloway <agalloway@thesoftwareguild.com>
2020-07-08 16:05:34 -0400

    updates to projects

--- DebugCodeAlong/src/main/java/com/aspire/debugcodealong/AnswerGame.java
+++ DebugCodeAlong/src/main/java/com/aspire/debugcodealong/AnswerGame.java
@@ -8,16 +8,24 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author agalloway
  */
 public class AnswerGame {
+
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
+        //Scanner myScanner = new Scanner(System.in);
         System.out.print("\nAnswer Game ");
-        String test = myScanner.nextLine();
+        //String test = myScanner.nextLine();
+        boolean run = true;
+        int times = 0;
+        do {
+            System.out.println("test");
+            run = times++ < 10;
+        } while (run);
+
+        
     }
-    
+
 }
