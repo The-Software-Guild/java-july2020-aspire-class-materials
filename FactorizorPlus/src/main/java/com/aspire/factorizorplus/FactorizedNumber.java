2fe171b18b203b25de116dc367756a43afd21665
Alan Galloway <agalloway@thesoftwareguild.com>
2020-07-14 11:24:16 -0400

    FactorizorPlus code along - updates

--- FactorizorPlus/src/main/java/com/aspire/factorizorplus/FactorizedNumber.java
+++ FactorizorPlus/src/main/java/com/aspire/factorizorplus/FactorizedNumber.java
@@ -11,7 +11,57 @@ package com.aspire.factorizorplus;
  */
 public class FactorizedNumber {
 
-    @Override
+    private final int number;
+    private final int[] factors;
+    private final boolean isPrime;
+    private final boolean isPerfect;
+
+    public FactorizedNumber(int providedNumber) {
+        number = providedNumber;
+        int[] tempFactors = new int[number];
+        int count = 1;
+        tempFactors[0] = 1;
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                tempFactors[count] = i;
+                count++;
+            }
+
+        }
+        //add one to count for the factored number
+        count++;
+        int[] result = new int[count];
+        System.arraycopy(tempFactors, 0, result, 0, result.length);
+        result[result.length - 1] = number;
+        factors = result;
+
+        int sum = 0;
+        for (int i = 0; i < factors.length - 1; i++) {
+            sum += factors[i];
+        }
+        isPerfect = (sum == number);
+
+        isPrime = (factors.length == 2);
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int[] getFactors() {
+        return factors;
+    }
+
+    public boolean isPrime() {
+        return isPrime;
+    }
+
+    public boolean isPerfect() {
+        return isPerfect;
+    }
+    
+        @Override
     public String toString() {
 
         String formattedOutput;
@@ -38,58 +88,6 @@ public class FactorizedNumber {
 
         return formattedOutput;
         //return "FactorizedNumber{" + "number=" + number + ", factors=" + factors + ", isPrime=" + isPrime + ", isPerfect=" + isPerfect + '}';
-    }
-
-    private final int number;
-    private final int[] factors;
-    private final boolean isPrime;
-    private final boolean isPerfect;
-
-    public FactorizedNumber(int providedNumber) {
-        number = providedNumber;
-        int[] tempFactors = new int[number];
-        int count = 1;
-        tempFactors[0] = 1;
-        for (int i = 2; i <= number / 2; i++) {
-            if (number % i == 0) {
-                tempFactors[count] = i;
-                count++;
-            }
-
-        }
-        //add one to count for the factored number
-        count++;
-        int[] result = new int[count];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = tempFactors[i];
-        }
-        result[result.length - 1] = number;
-        factors = result;
-
-        int sum = 0;
-        for (int i = 0; i < factors.length - 1; i++) {
-            sum += factors[i];
-        }
-        isPerfect = (sum == factors[factors.length - 1]);
-
-        isPrime = (factors.length == 2);
-
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public int[] getFactors() {
-        return factors;
-    }
-
-    public boolean isPrime() {
-        return isPrime;
-    }
-
-    public boolean isPerfect() {
-        return isPerfect;
     }
 
 }
